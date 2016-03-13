9d1d5adf48b4ac24726ecbf7c9f21d3f159fb9c1
Rafał Pucia <rafalpucia95@gmail.com>
2016-03-13 13:11:32 +0100

    Dodany kontroler i małe wyjaśnienie

--- Nauka/Nauka/Controllers/HomeController.cs
+++ Nauka/Nauka/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nauka.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,11 +27,24 @@ namespace Nauka.Controllers
 
             return View();
         }
+
+        //GET: Jak nie napiszesz nic przed akcją to domyślnie wywoływana jest metoda GET. O metodach HTTP można poczytać w necie.
         public ActionResult Wololo()
         {
             ViewBag.Message = "Nowy przycisk na miarę moich możliwości";
 
             return View();
+        }
+
+        //POST: Jeśli chcemy wywołać metodę POST to dodajemy przed akcją dopisek specjalny.
+        [HttpPost]
+        public ActionResult Wololo(RegisterUserViewModel model) //Dodatkowo jako parametr przyjmuje ten model który utworzyłeć specjalnie dla tej akcji w pliku AccountViewModels.cs
+        {
+            //Tutaj trzeba utworzyć nowy obiekt jakiegoś typu, przypisać do niego wartości z modelu pobranego przez parametr
+            //I można go zapisać do bazy danych
+            //Ta metoda POST wywoływana jest wtedy jak coś wysyłamy na serwer z przeglądarki.
+            //Najczęściej po kliknięciu submit, register czy coś takiego
+            return View(model);
         }
     }
 }
